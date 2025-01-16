97eadc482be98adc0c8e5f568ede6886c5d70b90
Ashe Aupepin sudosmile <ashe@noi.se.net>
2025-01-16 20:13:13 +0100

    clean + install on non wayland

---
+++ clean.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+install_db=$HOME/.local/share/kaomojis/kaomoji.tsv
+install_desktop=$HOME/.local/share/applications/kaokao.desktop
+install_bin=$HOME/.local/bin/kaokao.sh
+
+rm "$install_bin" "$install_db" "$install_desktop"
--- kaokao.sh
+++ kaokao.sh
@@ -3,12 +3,20 @@
 # simple script - allow selecting from kaomoji.tsv
 # and copies selection to system cliboard
 kaomojis=$HOME/.local/share/kaomojis/kaomoji.tsv
+rofi=""
+
+if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
+    rofi="wofi --dmenu -i"
+else
+    rofi="rofi -dmenu -i"
+fi
 
 if [ -e $kaomojis ]; then
-    wofi --dmenu -i < "$kaomojis" | cut -f 1 | wl-copy
+    $rofi < "$kaomojis" | cut -f 1 | wl-copy
 else
     echo "kaomoji.tsv not found, try running install.sh"
     exit 1
 fi
+
 
 # TODO: check if running X11 or wayland, use rofi or wofi
